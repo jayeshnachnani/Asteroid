e75d3c25cf788adafa9caf93121a2c4a36af4d7a
Jayesh Nachnani <jayesh.nachnani@gmail.com>
2021-02-06 21:11:17 +0530

    Fetch Data Worker v1

--- app/src/main/java/com/udacity/asteroidradar/work/FetchDataWorker.kt
+++ app/src/main/java/com/udacity/asteroidradar/work/FetchDataWorker.kt
@@ -17,86 +17,39 @@ import retrofit2.Response
 import timber.log.Timber
 import kotlin.coroutines.CoroutineContext
 
-class FetchDataWorker(appContext: Context, params: WorkerParameters):
+class FetchDataWorker(appContext: Context, params: WorkerParameters) :
     CoroutineWorker(appContext, params) {
 
     companion object {
         const val WORK_NAME = "FetchDataWorker"
     }
+
     val dataSource = AsteroidDatabase.getInstance(applicationContext).asteroidDatabaseDao
     val coroutineScope = CoroutineScope(Dispatchers.IO)
+    var asteroidList = ArrayList<Asteroid>()
 
-    /*override suspend fun doWork(): Result {
-        //TODO("Not yet implemented")
-        //val dataSource = AsteroidDatabase.getInstance(applicationContext).asteroidDatabaseDao
-        //val database = getDatabase(applicationContext)
-        //val repository = VideosRepository(database)
-        return try {
-            dataSource.getAllAsteroids()
-            Result.success()
-        } catch (e: HttpException) {
-            Result.retry()
-        }
-    }*/
 
     override suspend fun doWork(): Result {
         return try {
-            val response = AsteroidApi.retrofitService.getProperties()
-            //if response.
+            val response: Response<String> = AsteroidApi.retrofitService.getProperties().execute()
+            var obj1 = JSONObject(response.body().toString())
+            asteroidList = parseAsteroidsJsonResult(obj1)
+            coroutineScope.launch {
+                withContext(Dispatchers.IO)
+                {
+                    asteroidList.forEach { dataSource.insert(it) }
 
+                }
+            }
             Result.success()
         } catch (e: HttpException) {
             Result.retry()
         }
     }
-        /*AsteroidApi.retrofitService.getProperties().enqueue( object: Callback<String> {
-            override  fun onFailure(call: Call<String>, t: Throwable) {
-                //_response.value = "Failure: " + t.message
-                //Timber.i("Failure:" + _response.value)
-                Result.failure()
-            }
-
-            override  fun onResponse(call: Call<String>, response: Response<String>) {
-                //_response.value = "Success: ${response.body()} Asteroid properties retrieved"
-                var obj1 = JSONObject(response.body().toString())
-                Timber.i("obj1:" + obj1.toString())
-                var asteroidList = ArrayList<Asteroid>()
-                asteroidList = parseAsteroidsJsonResult(obj1)
-                asteroidList.forEach {  "Timbuk:" + Timber.i("TimbukTimbuk" + it.codename) }
-                asteroidList.forEach {
-                    //dataSource.insert(it)
-                    //_list.add(it)
-                }
-                coroutineScope.launch {
-                    withContext(Dispatchers.IO)
-                    {
-                        //dataSource.insert(asteroid)
-                    test()}
-                }
-                //viewModelScope.launch {
-                    //test()
-                    //insertAsteroidsToDatabase()
-                //}
-                val asteroid1 = Asteroid(1L,"second","sec",1.6,1.7,
-                    1.8,1.9,true)
-                //asteroidList.add(asteroid1)
-                /*dataSource.insert(asteroid1)
-                Timber.i("Data inserted")
-                val asterList = dataSource.getAllAsteroids()
-                asterList.value?.forEach {"Timbuk2:" + Timber.i(it.codename) }*/
-              Result.success()
-            }
-        })
-
-
-        //_response.value = "Set the Mars API Response here!"
-    }*/
-
-    private suspend fun test() {
+    /*private suspend fun test() {
         withContext(Dispatchers.IO) {
             //dataSource.insert(asteroid)
 
         }
-
-    }
+    }*/
 }
